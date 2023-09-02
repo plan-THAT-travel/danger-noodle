af2f5bdd36ae1c74b479d1b7a7785fe13582c476
Rachel Power <96928310+rpower15@users.noreply.github.com>
2023-09-02 07:13:08 -0600

    Moved app to index.js

--- client/index.js
+++ client/index.js
@@ -1,9 +1,37 @@
+/**
+ * ************************************
+ *
+ * @module  index.js
+ * @author  Plan It Travel
+ * @date    9-1-23
+ * @description entry point for application. Hangs React app off of #root in index.html
+ *
+ * ************************************
+ */
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Trip from './pages/Trip';
 import store from './store';
 // import styles from './scss/application.scss'; // eslint-disable-line no-unused-vars
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />} />
+        <Route index element={<Login />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/Trip' element={<Trip />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = createRoot(document.getElementById('root'));
 root.render(
