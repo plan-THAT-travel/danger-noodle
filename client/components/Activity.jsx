2d79a3bf52f5cb261d240bb8cc0368add1144bba
Jason Pennini <jason.pennini@gmail.com>
2023-09-04 19:54:47 -0700

    Activity updated

---
+++ client/components/Activity.jsx
@@ -0,0 +1,10 @@
+/**
+ * ************************************
+ *
+ * @module  Activity.jsx
+ * @author  Plan It Travel
+ * @date    9-4-23
+ * @description presentation component that renders a feed list item for each activity
+ *
+ * ************************************
+ */
